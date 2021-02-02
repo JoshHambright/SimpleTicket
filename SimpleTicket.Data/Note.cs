0552f3bfed14e3e850071d822e365c6478b7595f
JoshHambright <josh.hambright@gmail.com>
2021-02-02 13:59:55 -0500

    note class

---
+++ SimpleTicket.Data/Note.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTicket.Data
+{
+    public class Note
+    {
+        [Key]
+        public int ID { get; set; }
+        public Guid Creator { get; set; }
+        [DataType(DataType.MultilineText)]
+        public string Body { get; set; }
+
+        public Guid TicketID { get; set; }
+        public virtual Ticket Ticket { get; set; }
+    }
+}
