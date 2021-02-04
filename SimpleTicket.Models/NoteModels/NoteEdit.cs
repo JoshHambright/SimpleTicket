4ff9fdeb8c7588070ec7bdd575d2eda14591b3a3
JoshHambright <josh.hambright@gmail.com>
2021-02-04 14:17:46 -0500

    note models

---
+++ SimpleTicket.Models/NoteModels/NoteEdit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTicket.Models.NoteModels
+{
+    public class NoteEdit
+    {
+        [Required]
+        public int ID { get; set; }
+        [Required]
+        public Guid Creator { get; set; }
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string Body { get; set; }
+        public DateTimeOffset DateCreate { get; set; }
+        public DateTimeOffset? DateUpdated { get; set; }
+        [Required]
+        public Guid TicketID { get; set; }
+    }
+}
---
+++ SimpleTicket.Models/NoteModels/NoteList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTicket.Models.NoteModels
+{
+    class NoteList
+    {
+        public int ID { get; set; }
+        public string CreatorName { get; set; }
+        public string BodyShort { get; set; }
+        public DateTimeOffset DateCreate { get; set; }
+        public DateTimeOffset? DateUpdated { get; set; }
+
+    }
+}
